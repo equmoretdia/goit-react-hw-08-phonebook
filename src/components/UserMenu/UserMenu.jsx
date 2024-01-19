88c751a4d657047cac527be8c21696ba2df58ba7
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-19 23:00:36 +0100

    UserMenu component (and its basic styling) is added

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { logOut } from '../../redux/auth/operations';
+import { selectUser } from '../../redux/auth/operations';
+import defaultAvatar from '../../img/auth/default-avatar.png';
+
+import css from './UserMenu.module.css';
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  return (
+    <div className={css.container}>
+      <img
+        src={defaultAvatar}
+        alt="funny smiling penguin avatar"
+        width="32"
+        className={css.avatar}
+      />
+      <span className={css.name}>Wellcome, {user.name}</span>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Quit
+      </button>
+    </div>
+  );
+};
+
+export default UserMenu;
