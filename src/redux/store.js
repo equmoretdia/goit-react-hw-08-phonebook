33bf28ec50378e802b803cde1ff9b4a499d2842e
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-19 21:30:11 +0100

    store is updated with new auth persistReducer, auth selectors are added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,39 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { contactsReducer } from './contacts & filter/contactsSlice';
 import { filterReducer } from './contacts & filter/filterSlice';
+import { authReducer } from './auth/authSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
