776af6d88da38ad353e8de98173f232bccc9d3c5
Serhiy Belinskyy <134813907+equmoretdia@users.noreply.github.com>
2024-01-20 19:27:01 +0100

    Navigation is updated: link to Contacts is visible now only in case of user is logged in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,20 +1,24 @@
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 import css from './Navigation.module.css';
 
-const Navigation = () => (
-  <ul className={css.list}>
-    <li>
+const Navigation = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return (
+    <nav className={css.nav}>
       <NavLink className={css.link} to="/">
         Home
       </NavLink>
-    </li>
-    <li>
-      <NavLink className={css.link} to="/contacts">
-        Contacts
-      </NavLink>
-    </li>
-  </ul>
-);
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
+    </nav>
+  );
+};
 
 export default Navigation;
